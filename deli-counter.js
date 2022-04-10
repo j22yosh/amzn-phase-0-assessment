fc6e208250f00762c6a7bc120706c07ed17e5132
Joshua <j22yosh@gmail.com>
2022-04-10 13:16:23 -0400

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -3,22 +3,47 @@
 
 // 2. Example Usage
 
-// const katzDeli = []
+ const katzDeli = []
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
+ function line (array){
+   for(let x = 0; x < array.length; x++){
+  console.log("The line is currently: "+ `${x+1}` + ". " + `${array[x]}` )
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+    if (array.length === 0){
+        return console.log("The line is currently empty.")}}
+    
+ }
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+ function takeANumber(array, name){
+katzDeli.push(name)
+   return console.log("Welcome, " + `${name}` + ". You are number " + `${array.length}` + " in line.")
+     
+ }
+ 
+function nowServing(array){
+  console.log("Currently serving " + `${array[0]}`)
+  array.splice(0, 1);
+  if(array.length === 0){return console.log("There is nobody currently waiting to be served!")}
 
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
+}
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
-// nowServing(katzDeli) //=> "Currently serving Grace."
+takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
+takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
+takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+
+line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+nowServing(katzDeli) //=> "Currently serving Ada."
+
+line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+
+takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
+
+line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+ nowServing(katzDeli) //=> "Currently serving Grace."
+
+ line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
